b86a49ab6b8bc81a9c05b0779da7059682fa9044
memochiko <franciscocarreno1502@gmail.com>
2025-06-15 02:58:04 -0400

    Ainhoa

---
+++ config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    token: 'TU TOKEN',
+    clientId: 'TU ID',
+    guildId: '',
+    ownerId: 'TU ID DE USER',
+    welcomeChannelId: 'CANAL WLC',
+    farewellChannelId: 'CANAL GDB',
+    boostChannelId: 'CANAL BOOST',
+    weatherApiKey: '7cbca5f17785969ddca946edaeecff1a', // Para el comando /weather (ej. OpenWeatherMap)
+    googleTranslateApiKey: 'AIzaSyBuFJaC7I66LKpdbc7wZsIE_QobqtUpiVg' // Para el comando /translate
+};
